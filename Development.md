d6a29c48c940eecf98303e40d96fddb75f4bb5fe
SLaks <Dev@SLaks.net>
2014-06-26 08:25:49 -0400

    Add core game code

---
+++ Development.md
@@ -0,0 +1,15 @@
+﻿#Triangle grid
+The game board is a Cartesian grid of triangles.  To tessellate the triangles, every other triangle is inverted.  The origin, and all cells where  `x + y` is even, is upwards-pointing; odd cells are downwards-pointing.
+
+The sides of each triangle are identified by index.  `0` is the right side, `1` is the right side, and `2` is the vertical side (bottom for upwards-pointing; top for downwards-pointing).
+
+#Classes
+The classes in `game.js` track game state only and have no coupling to any UI or DOM elements.
+
+##Triangle class
+The `Triangle` class stores the contents of a triangle.  It just tracks the color of each side; it does not know where the triangle is or what orientation it is.
+
+##GameBoard class
+The `GameBoard` tracks the triangles in the board.  It stores every extant triangle, and it only allows mutations that result in a valid board.  (triangle orientations are simply computed from their locations)
+
+Individual triangles are passed as an array of `[x, y]`; these arrays are passed to and returned by methods in `GameBoard`.  Individual sides of a triangle are passed as arrays of `[x, y, index]`, where `index` is `0`, `1`, or `2`, as described above.
---
+++ javascript/game.js
@@ -0,0 +1,118 @@
+﻿'use strict';
+
+/**
+ * Stores the contents of a single triangle.
+ * @param {int[]}	sides The colors of each side in the triangle (right, left, vertical).
+ */
+function Triangle(sides) {
+	sides.sort();
+	this.sides = sides;
+}
+
+/**
+ * The number of distinct colors that the sides of a triangle can have.
+ */
+Triangle.colorCount = 6;
+
+Triangle.createRandom = function () {
+	// TODO: Only allow distinct sides?
+	return new Triangle([
+		Math.floor(Math.random() * Triangle.colorCount),
+		Math.floor(Math.random() * Triangle.colorCount),
+		Math.floor(Math.random() * Triangle.colorCount)
+	]);
+};
+
+
+/**
+ * Stores and manipulates all of the triangles in the game board.
+ */
+function GameBoard() {
+	this.contents = {};
+}
+
+
+/**
+ * Gets the string key in the contents map for a given location.
+ * @param {int[]}		location
+ */
+GameBoard.prototype.key = function(location) {
+	return location.join(',');
+};
+
+
+/**
+ * Gets the triangle at a given location.
+ * @param {int[]}		location
+ */
+GameBoard.prototype.get = function (location) {
+	return this.contents[this.key(location)];
+};
+
+
+/**
+ * Gets the color at the given side and location.
+ * @param {int[]}		locationSide
+ */
+GameBoard.prototype.getSide = function (locationSide) {
+	var triangle = this.get(locationSide);
+	if (!triangle)
+		return null;
+	return triangle.sides[locationSide[2]];
+};
+
+
+/**
+ * Gets the orientation of the given location.
+ * Returns 0 for an upwards-pointing triangle, or 1 for a downwards-pointing triangle.
+ * @param {int[]}		location
+ */
+GameBoard.prototype.orientation = function (location) {
+	return (location[0] + location[1]) % 2;
+};
+
+
+/**
+ * Gets the three sides adjacent to a given location.
+ * @param {int[]}		location
+ */
+GameBoard.prototype.getAdjacencies = function (location) {
+	var x = location[0], y = location[1];
+	var orientation = this.orientation(location);
+	return [
+		[x + 1, y, 1],
+		[x - 1, y, 0],
+		[x, y + (1 - 2 * orientation), 2]
+	];
+};
+
+
+/**
+ * Checks whether the specified triangle is allowed at the specified location.
+ * 
+ * @param {int[]}		location
+ * @param {Triangle}	triangle
+ */
+GameBoard.prototype.isAllowed = function (location, triangle) {
+	return this.getAdjacencies(location)
+		.map(this.getSide.bind(this))
+		.every(function (color, index) {
+			return color === null || color === triangle.sides[index];
+		});
+};
+
+
+/**
+ * Adds a new triangle at the specified location.
+ * 
+ * @param {int[]}		location
+ * @param {Triangle}	triangle
+ */
+GameBoard.prototype.add = function (location,triangle ) {
+	if (this.get(location))
+		throw new Error('Cannot overwrite existing triangle');
+	if (!this.isAllowed(location, triangle))
+		throw new Error('Cannot insert mismatching triangle');
+
+	this.contents[this.key(location)] = triangle;
+};
